58be40128f4e95decf6aeda35b641ddc678b1363
+ <+>
2021-09-10 12:11:21 -0700

    list out courses and course show page

--- src/components/Course.js
+++ src/components/Course.js
@@ -6,6 +6,18 @@ class Course {
         this.constructor.all.push(this);
     }
 
+    renderShow = () => {
+        const {name, description, id} = this.data;
+        document.getElementById("main").innerHTML = `
+        <div class="show">
+            <h1>${name}</h1>
+            <p>${description}</p>
+            <a href="#" id="back" class="btn btn-primary">Back</a>
+        </div>
+        `
+        document.getElementById("back").addEventListener("click", Course.renderIndex);
+    }
+
     renderCard = () => {
         const {name, description, id} = this.data;
         document.getElementById("course-row").innerHTML += `
@@ -14,13 +26,23 @@ class Course {
                 <div class="card-body">
                     <div class="card-title">${name}</div>
                     <div class="card-text">${description}</div>
-                    <a href="#" class="btn btn-primary">View Course</a>
+                    <a href="#" id="view-course" data-id="${id}" class="btn btn-primary">View Course</a>
                 </div>
             </div>
         </div>`
+        document.querySelectorAll(`[data-id]`).forEach(button => {
+            button.addEventListener("click", Course.handleIndexClick);
+        });
     }
 
-    static renderIndex() {
+    static find = (id) => this.all.find(course => course.data.id == id);
+
+    static handleIndexClick = (e) => {
+        const id = e.target.dataset.id;
+        this.find(id).renderShow();
+    }
+
+    static renderIndex = () => {
         const courseContainer = document.createElement("div");
         courseContainer.classList.add("course-container");
 
@@ -29,11 +51,13 @@ class Course {
         courseRow.id = "course-row";
         courseContainer.appendChild(courseRow);
         
-        document.getElementById("main").appendChild(courseContainer);
+        const main = document.getElementById("main");
+        main.innerHTML = "";
+        main.appendChild(courseContainer);
         this.all.forEach(course => course.renderCard());
     }
 
-    static getCourses() {
+    static getCourses = () => {
         api.getCourses().then(courses => {
             courses.forEach(course => new Course(course));
             this.renderIndex();
